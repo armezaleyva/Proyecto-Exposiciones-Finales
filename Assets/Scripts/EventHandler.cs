6c776970c89ac60267fa9153549efd463b255226
Armando Meza Leyva <armezaleyva@gmail.com>
2019-05-14 12:28:28 -0700

    Comenzar a crear la estructura y bases para los eventos

--- Assets/Scripts/EventHandler.cs
+++ Assets/Scripts/EventHandler.cs
@@ -6,16 +6,53 @@ using UnityEngine.UI;
 public class EventHandler : MonoBehaviour
 {
     [SerializeField]
+    float tiempoDeEspera = 8;
+    [SerializeField]
     GameObject panel;
+    [SerializeField]
+    Text textoEvento;
+    List<Evento> listaEventos;
 
     // Start is called before the first frame update
     void Start()
     {
-        panel.active = false;
+        panel.SetActive(true);
+        Evento evento0 = new Evento(
+            0,
+            "ola",
+            "avia una vez un wei",
+            "imon",
+            "nel"
+        );
+        Evento evento1 = new Evento(
+            1,
+            "hola",
+            "havia una vez un wei",
+            "himon",
+            "nhehl"
+        );
+
+        listaEventos = new List<Evento>();
+        listaEventos.Add(evento0);
+
+        StartCoroutine(TimerEvento());
     }
 
     // Update is called once per frame
     void Update()
     {
+        textoEvento.text = "Wereja";
+    }
+
+    IEnumerator TimerEvento()
+    {
+        yield return new WaitForSeconds(tiempoDeEspera);
+        GenerarEvento();
+    }
+
+    void GenerarEvento() 
+    {
+        print("xd");
+
     }
 }
---
+++ Assets/Scripts/Evento.cs
@@ -0,0 +1,18 @@
+﻿public class Evento
+{
+    public int idEvento;
+    public string nombreEvento;
+    public string descripcionEvento;
+    public string decision1;
+    public string decision2;
+
+    public Evento(int idEvento, string nombreEvento, string descripcionEvento,
+            string decision1, string decision2) 
+    {
+        this.idEvento = idEvento;
+        this.nombreEvento = nombreEvento;
+        this.descripcionEvento = descripcionEvento;
+        this.decision1 = decision1;
+        this.decision2 = decision2;
+    }
+}
